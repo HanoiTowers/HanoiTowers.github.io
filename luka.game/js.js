01574e32bfa8d4c154b655cd8e71738d7f34d63a
gammlet <37348223+gammlet@users.noreply.github.com>
2021-07-20 16:39:27 +0300

    Update js.js

--- luka.game/js.js
+++ luka.game/js.js
@@ -1,23 +1,21 @@
-  function pressButton1(){
-    ctx.clearRect(0, 175, 125, 25)
+ 
+   function pressButton1(){
+    ctx.clearRect(125, 175, 125, 25)
+    ctx.clearRect(275, 175, 125, 25)
     ctx.fillStyle='blue'
-    ctx.fillRect (125, 175, 125, 25)
-    ctx.fillStyle='green' 
-    ctx.fillRect (50,175, 25,25)
+    ctx.fillRect (0, 175, 125, 25)
    }
    function pressButton2(){
     ctx.clearRect(0, 175, 125, 25)
+    ctx.clearRect(275, 175, 125, 25)
     ctx.fillStyle='blue'
     ctx.fillRect (125, 175, 125, 25)
-    ctx.fillStyle='green' 
-    ctx.fillRect (50,175, 25,25)
    }
    function pressButton3(){
     ctx.clearRect(0, 175, 125, 25)
+    ctx.clearRect(125, 175, 125, 25)
     ctx.fillStyle='blue'
     ctx.fillRect (275, 175, 125, 25)
-    ctx.fillStyle='green' 
-    ctx.fillRect (50,175, 25,25)
    }
    
  var
